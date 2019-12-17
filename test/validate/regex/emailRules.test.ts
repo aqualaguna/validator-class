0bcbbbe88a847760d788df97637270872f9ebf61
eventnia <bseigekit@gmail.com>
2019-12-17 13:50:37 +0700

    emailRules test and pass

---
+++ test/validate/regex/emailRules.test.ts
@@ -0,0 +1,58 @@
+import Validator from '../../../src/index';
+
+describe('emailRules', () => {
+
+
+
+  it('should success email rules', async () => {
+    let data = {
+      val1: "test@yahoo.com",
+      val2: "test.yahoo.com",
+      val3: "test98;yahoo;com",
+      val4: "test98@yahoo@com",
+      val5: "t3st/yahoo/com",
+      val6: true,
+      val7: 98,
+      val8: [],
+      val9: {}
+    };
+    let rules = {
+        val1: "email", // true
+        val2: "email", // false
+        val3: "email", // false
+        val4: "email", // false
+        val5: "email", // false
+        val6: "email", // false
+        val7: "email", // false
+        val8: "email", // false
+        val9: "email" // false
+    };
+    let t = new Validator(data, rules);
+    let err = t.validate();
+    expect(Object.keys(err).length).toBe(8);
+    expect(err).toEqual(expect.objectContaining({
+      
+    }))
+  });
+
+  it('nested email', async () => {
+    let data = {
+      temp: {
+        val1: "test@yahoo.com",
+        val2: "test@yahoo@com",
+      }
+    };
+    let rules = {
+      temp: {
+        val1: 'email', // unexpected rule
+        val2: 'email' // invalid rule
+      }
+    };
+    let t = new Validator(data, rules);
+    let err = t.validate();
+    expect(Object.keys(err).length).toBe(1);
+    expect(err).toEqual(expect.objectContaining({
+
+    }))
+  });
+});
